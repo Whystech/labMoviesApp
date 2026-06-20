b7479e6c53b48bbf849f1cdda9554a19060790ae
Dicblender <andrew.theodor@gmail.com>
2026-06-20 13:06:20 +0300

    Movie Details  page components.

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -21,3 +21,9 @@ export interface BaseMovieProps {
     movies: BaseMovieProps[];
   }   
 
+    export interface MovieDetailsProps extends BaseMovieProps {
+    genres: {
+      id: number;
+      name: string;
+    }[];
+  }

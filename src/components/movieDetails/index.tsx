dce69f2f7a54a14a98d00938065f9bcbdbf51ea7
Dicblender <andrew.theodor@gmail.com>
2026-06-20 13:20:45 +0300

    Movie Details Page - included production countries.

--- src/components/movieDetails/index.tsx
+++ src/components/movieDetails/index.tsx
@@ -8,55 +8,72 @@ import Typography from "@mui/material/Typography";
 import { MovieDetailsProps } from "../../types/interfaces";
 
 const styles = {
-    chipSet: {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexWrap: "wrap",
-        listStyle: "none",
-        padding: 1.5,
-        margin: 0,
-    },
-    chipLabel: {
-        margin: 0.5,
-    },
+  chipSet: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexWrap: "wrap",
+    listStyle: "none",
+    padding: 1.5,
+    margin: 0,
+  },
+  chipLabel: {
+    margin: 0.5,
+  },
 };
 
 const MovieDetails: React.FC<MovieDetailsProps> = (movie) => {
+  return (
+    <>
+      <Typography variant="h5" component="h3">
+        Overview
+      </Typography>
 
-    return (
-        <>
-            <Typography variant="h5" component="h3">
-                Overview
-            </Typography>
+      <Typography variant="h6" component="p">
+        {movie.overview}
+      </Typography>
 
-            <Typography variant="h6" component="p">
-                {movie.overview}
-            </Typography>
+      <Paper component="ul" sx={styles.chipSet}>
+        <li>
+          <Chip label="Genres" sx={styles.chipLabel} color="primary" />
+        </li>
+        {movie.genres.map((g) => (
+          <li key={g.name}>
+            <Chip label={g.name} />
+          </li>
+        ))}
 
-            <Paper component="ul" sx={styles.chipSet}>
-                <li>
-                    <Chip label="Genres" sx={styles.chipLabel} color="primary" />
-                </li>
-                {movie.genres.map((g) => (
-                    <li key={g.name}>
-                        <Chip label={g.name} />
-                    </li>
-                ))}
-            </Paper>
-            <Paper component="ul" sx={styles.chipSet}>
-                <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
-                <Chip
-                    icon={<MonetizationIcon />}
-                    label={`${movie.revenue.toLocaleString()}`}
-                />
-                <Chip
-                    icon={<StarRate />}
-                    label={`${movie.vote_average} (${movie.vote_count}`}
-                />
-                <Chip label={`Released: ${movie.release_date}`} />
-            </Paper>
-        </>
-    );
+        {movie.production_countries.map((c) => (
+          <li key={c.name}>
+            <Chip label={c.name} />
+          </li>
+        ))}
+      </Paper>
+
+      <Paper component="ul" sx={styles.chipSet}>
+        <li>
+          <Chip label="Countries" sx={styles.chipLabel} color="primary" />
+        </li>
+
+        {movie.production_countries.map((c) => (
+          <li key={c.name}>
+            <Chip label={c.name} />
+          </li>
+        ))}
+      </Paper>
+      <Paper component="ul" sx={styles.chipSet}>
+        <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
+        <Chip
+          icon={<MonetizationIcon />}
+          label={`${movie.revenue.toLocaleString()}`}
+        />
+        <Chip
+          icon={<StarRate />}
+          label={`${movie.vote_average} (${movie.vote_count}`}
+        />
+        <Chip label={`Released: ${movie.release_date}`} />
+      </Paper>
+    </>
+  );
 };
 export default MovieDetails;

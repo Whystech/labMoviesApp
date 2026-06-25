19867d412f2e1550d18e03df7c436db808370d3d
Andrei Ianus <andreiianus@microsoft.com>
2026-06-25 18:06:57 +0300

    Added Upcoming movies page.

--- src/index.tsx
+++ src/index.tsx
@@ -5,24 +5,20 @@ import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
+import SiteHeader from "./components/siteHeader";
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/movies/favourites">Favourites</Link>
-        </li>
-      </ul>
+      <SiteHeader />      {/* New Header  */}
       <Routes>
         <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/" />} />
-         <Route path="/reviews/:id" element={<MovieReviewPage/>} />
+        <Route path="/reviews/:id" element={<MovieReviewPage />} />
+        <Route path="/movies/upcoming" element={<UpcomingMoviesPage/>} />
 
       </Routes>
     </BrowserRouter>

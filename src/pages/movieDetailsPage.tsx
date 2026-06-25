e7eb7d77e82536f581d4d9529471ac77c4d54398
Andrei Ianus <andreiianus@microsoft.com>
2026-06-25 18:28:13 +0300

    Show movie review excerpts; Refactor Details page.

--- src/pages/movieDetailsPage.tsx
+++ src/pages/movieDetailsPage.tsx
@@ -1,18 +1,26 @@
-import React from "react"; // replace existing react import
+import React, {useState, useEffect}  from "react"; // replace existing react import
 import { useParams } from "react-router-dom";
 import MovieDetails from "../components/movieDetails";
+import { MovieDetailsProps} from "../types/interfaces";
+import { getMovie} from "../api/tmdb-api";
 import PageTemplate from "../components/templateMoviePage";
-import useMovie from "../hooks/useMovie";
+
 
 const MovieDetailsPage: React.FC= () => {
   const { id } = useParams();
-  const [movie] = useMovie(id ?? "");
+  const [movie, setMovie] = useState<MovieDetailsProps>();
+
+  useEffect(() => {
+    getMovie(id ?? "").then((movie) => {
+      setMovie(movie);
+    });
+  }, [id]);
 
   return (
     <>
       {movie ? (
         <>
-        <PageTemplate movie={movie}> 
+        <PageTemplate movie={movie}>
           <MovieDetails {...movie} />
         </PageTemplate>
       </>
